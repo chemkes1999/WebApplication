91c7ce2a49196ae16f953556e0665cda1e5ddff3
Carlos Hemkes <c.hemkes1999@gmail.com>
2023-07-15 12:02:05 -0600

    inicial

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Esta es una prueba de mi primera apicacion con ASP.NET.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Mi contacto.";
+            return View();
+        }
+    }
+    
+}
